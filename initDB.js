28725f97cb28ff9b9675b3e73dbf1eb5a9241a18
Alex Young <younga6@oregonstate.edu>
2022-06-02 19:46:30 -0700

    merged with Christian's branch and updated initDB to create courses

--- initDB.js
+++ initDB.js
@@ -3,17 +3,24 @@
  * data from the files in the data/ directory.
  */
 
-const sequelize = require('./lib/sequelize')
-const { User, UserClientFields } = require('./models/user')
-const { Submission, SubmissionClientFields } = require('./models/submission')
-const { Assignment, AssignmentClientFields } = require('./models/assignment')
+const sequelize = require('./lib/sequelize');
+const { User, UserClientFields } = require('./models/user');
+const { Submission, SubmissionClientFields } = require('./models/submission');
+const { Assignment, AssignmentClientFields } = require('./models/assignment');
+const { Course, CourseClientFields } = require('./models/course');
 
-const userData = require('./data/users.json')
-const submissionData = require('./data/submissions.json')
-const assignmentData = require('./data/assignments.json')
+const userData = require('./data/users.json');
+const submissionData = require('./data/submissions.json');
+const assignmentData = require('./data/assignments.json');
+const courseData = require('./data/courses.json');
 
 sequelize.sync().then(async function () {
-  await User.bulkCreate(userData, { fields: UserClientFields })
-  await Submission.bulkCreate(submissionData, { fields: SubmissionClientFields })
-  await Assignment.bulkCreate(assignmentData, { fields: AssignmentClientFields })
-})
+    await User.bulkCreate(userData, { fields: UserClientFields });
+    await Submission.bulkCreate(submissionData, {
+        fields: SubmissionClientFields,
+    });
+    await Assignment.bulkCreate(assignmentData, {
+        fields: AssignmentClientFields,
+    });
+    await Course.bulkCreate(courseData, { fields: CourseClientFields });
+});
