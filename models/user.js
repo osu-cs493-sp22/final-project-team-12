0f7497842f0729f2f4796c2e4198d6eeb5d0a5dd
Alex Young <younga6@oregonstate.edu>
2022-06-02 22:00:35 -0700

    added sequelize associations - not positive that this is correct

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@ const { DataTypes } = require('sequelize');
 
 const sequelize = require('../lib/sequelize');
 const bcrypt = require('bcryptjs');
+const { Course } = require('./course');
+const { Assignment } = require('./assignment');
+const { Submission } = require('./submission');
 
 const User = sequelize.define('user', {
     name: { type: DataTypes.STRING, allowNull: false },
@@ -15,6 +18,15 @@ const User = sequelize.define('user', {
     },
     role: { type: DataTypes.STRING, allowNull: false },
 });
+
+User.belongsToMany(Course, { through: 'Students' });
+Course.belongsToMany(User, { through: 'Students' });
+
+Assignment.hasMany(Submission, { foreignKey: { allowNull: false } });
+Submission.belongsTo(Assignment);
+
+Course.hasMany(Assignment, { foreignKey: { allowNull: false } });
+Assignment.belongsTo(Course);
 
 exports.User = User;
 exports.UserClientFields = ['name', 'email', 'password', 'role'];
