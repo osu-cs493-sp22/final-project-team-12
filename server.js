9c446c3e7423d3c0de0b734b94d30e56692c3116
Steven Bui <bui.stvn@gmail.com>
2022-06-04 16:29:57 -0700

    Implement rate limiting based on IP

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const api = require('./api');
+const { rateLimit } = require('./lib/rateLimit');
 const redisClient = require('./lib/redis');
 const sequelize = require('./lib/sequelize');
 
@@ -13,6 +14,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use(rateLimit);
 app.use('/', api);
 
 app.use('*', function (req, res, next) {
